9b9142bea33b9629dd830e88594227431445abe0
unknown <eapaniagua8@misena.edu.co>
2024-06-22 11:23:25 -0500

    Rastro del cursor

--- Assets/_Scripts/PointerFollow.cs
+++ Assets/_Scripts/PointerFollow.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PointerFollow : MonoBehaviour
 {
-    
-    
+
+    public Camera camera;
     
     // Start is called before the first frame update
     void Start()
@@ -16,6 +16,9 @@ public class PointerFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-            
+        Vector3 mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
+        mousePos = new Vector3(mousePos.x, mousePos.y);
+
+        transform.position = mousePos;
     }
 }
