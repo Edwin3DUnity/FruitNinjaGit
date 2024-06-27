4b14a23ebf0e2b83a22a6117a16f72e4c609bc0b
unknown <eapaniagua8@misena.edu.co>
2024-06-27 17:36:02 -0500

    Practica finalizada

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,161 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    [SerializeField]private float spawnRate = 1;
+    
+    public enum  GameState
+    {
+        loading, 
+        inpaused,
+        inGame,
+        gameOver
+        
+        
+    }
+
+    public GameState gameState;
+
+    public TextMeshProUGUI scoreText;
+
+    private int _score;
+
+    private int score
+    {
+        set
+        {
+            _score = Mathf.Max(value, 0);
+        }
+        get
+        {
+            return _score;
+        }
+        
+        
+        
+        
+    }
+
+    public TextMeshProUGUI gameOverText;
+    public Button restatButton;
+
+    public GameObject titleScreen;
+
+    private const string MAX_SCORE = "MAX_SCORE";
+
+    private int numberOfLives = 4;
+    public List<GameObject> lives;
+    
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       ShowMaxScore();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (gameState == GameState.inGame)
+        {
+           yield return new WaitForSeconds(spawnRate);
+           int index = Random.Range(0, targets.Count);
+           Instantiate(targets[index]);
+
+        }
+      
+    }
+
+
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score  \n " + score;
+    }
+
+    private void SetMaxScore()
+    {
+        int maxScore = PlayerPrefs.GetInt(MAX_SCORE, 0);
+        if (score > maxScore)
+        {
+            PlayerPrefs.SetInt(MAX_SCORE, score);
+        }
+    }
+
+
+    public void GameOver()
+    {
+        numberOfLives--;
+        if (numberOfLives >= 0)
+        {
+            Image heartImage = lives[numberOfLives].GetComponent<Image>();
+            var tempColor = heartImage.color;
+            tempColor.a = 0.3f;
+            heartImage.color = tempColor;
+
+        }
+
+        if (numberOfLives <= 0)
+        {
+            SetMaxScore();
+
+            gameState = GameState.gameOver;
+            
+            gameOverText.gameObject.SetActive(true);
+            restatButton.gameObject.SetActive(true);
+
+        }
+
+    }
+
+
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+
+    public void StartGame(int difficulty)
+    {
+        gameState = GameState.inGame;
+        titleScreen.gameObject.SetActive(false);
+
+        spawnRate /= difficulty;
+        numberOfLives -= difficulty;
+
+
+        for (int i = 0; i < numberOfLives; i++)
+        {
+            lives[i].SetActive(true);
+        }
+
+        StartCoroutine(SpawnTarget());
+
+        score = 0;
+        UpdateScore(0);
+        gameOverText.gameObject.SetActive(false);
+
+    }
+
+
+    public void ShowMaxScore()
+    {
+        int maxScore = PlayerPrefs.GetInt(MAX_SCORE, 0);
+        scoreText.text = "Max Score: \n " + maxScore;
+    }
+}
--- Assets/_Scripts/Targets.cs
+++ Assets/_Scripts/Targets.cs
@@ -1,21 +1,83 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Targets : MonoBehaviour
+{
+
 
     private Rigidbody _rigidbody;
+
+    [SerializeField] private float minForce = 16;
+    [SerializeField] private float maxForce = 18;
+
+    [SerializeField] private float maxTorque = 10;
+
+    [SerializeField] private float posX = 4;
+    [SerializeField] private float posY = -4;
+
+    public ParticleSystem explotion;
+
+    private GameManager _gameManager;
+
+    public int pointvalue;
     // Start is called before the first frame update
     void Start()
     {
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.AddForce(RandomForce(),ForceMode.Impulse);
+        _rigidbody.AddTorque(RandomTorque(),RandomTorque(),RandomTorque(), ForceMode.Impulse);
+
+        transform.position = RandomPosx();
+
+        _gameManager = FindObjectOfType<GameManager>();
         
-        
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minForce, maxForce);
+    }
+
+
+    private float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    private Vector3 RandomPosx()
+    {
+        return new Vector3(Random.Range(-posX, posX), posY);
+    }
+
+
+    private void OnMouseOver()
+    {
+        Destroy(gameObject);
+        Instantiate(explotion, transform.position, explotion.transform.rotation);
+        _gameManager.UpdateScore(pointvalue);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("KillZone"))
+        {
+            Destroy(gameObject);
+        }
+
+        if (gameObject.CompareTag("Good"))
+        {
+            _gameManager.GameOver();
+        }
     }
 }
